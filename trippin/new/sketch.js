5de1f8b5030d9f889c510e425d8b79a58324f597
Karen <karenpenglabs@gmail.com>
2014-08-03 16:47:31 -0400

    weird scope

--- trippin/new/sketch.js
+++ trippin/new/sketch.js
@@ -1,30 +1,47 @@
-	window.onload = function () {
-	  // Get a reference to the canvas object
-	  var canvas = document.getElementById('myCanvas');
-	  // Create an empty project and a view for the canvas:
-	  paper.setup(canvas);
-	  // Create a Paper.js Path to draw a line into it:
-	  var path = new paper.Path();
-	  // Give the stroke a color
-	  path.strokeColor = 'black';
-	  var start = new paper.Point(100, 100);
-	  // Move to start and draw a line from there
-	  path.moveTo(start);
-	  // Note that the plus operator on Point objects does not work
-	  // in JavaScript. Instead, we need to call the add() function:
-	  path.lineTo(start.add([200, -50]));
-	  // Draw the view now:
-	  paper.view.draw();
-	}
+//paper.install(window);
 
-	function hell() {
-	  //this is about making a path
-	}
+var canvas, path, tool;
+// tool;
 
-	function whatever() {
-	  //this is about getting value from the path
-	}
+function init() {
+  // Get a reference to the canvas object
+  canvas = document.getElementById('myCanvas');
+  // Create an empty project and a view for the canvas:
+  paper.setup(canvas);
+  // Create a Paper.js Path to draw a line into it:
+  path = new paper.Path();
+  // Give the stroke a color
 
-	function hookup() {
-	  //this is about hookup to control animations
-	}
+  tool = new paper.Tool();
+
+  path.strokeColor = 'black';
+
+}
+
+tool.onMouseDown = function (e) {
+  path.add(e.point);
+}
+
+function update() {
+  if (path.segments.length > 0) {
+    path.forEach(function (s) {
+      console.log(s)
+    })
+    // var start = new paper.Point(100, 100);
+    // path.moveTo(start);
+    // // Note that the plus operator on Point objects does not work
+    // // in JavaScript. Instead, we need to call the add() function:
+    // path.lineTo(start.add([200, -50]));
+    // // Draw the view now:
+    // paper.view.draw();
+  }
+}
+
+function createAnimation(callback) {
+  requestAnimationFrame(update);
+  callback();
+}
+window.onload = function () {
+  init();
+  createAnimation(update);
+}

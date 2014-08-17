f5c321df089afe260572402350508652b8a9c9be
Karen <karenpenglabs@gmail.com>
2014-08-17 16:25:11 -0400

    try define paperScope manually

--- trippin/new/sketch.js
+++ trippin/new/sketch.js
@@ -1,17 +1,70 @@
-paper.install(window);
+//paper.install(window);
+var width = 640;
+var height = 480;
+var radius = 10;
+var beginX = radius;
+var beginY = radius;
+var endX = width - radius;
+var endY = height - radius;
+var interval = 6;
+var isDrawingDone = false;
+var time = 0;
+var booPadding = 20;
 
-//making a path
-window.onload = function () {
+function constrain(item, min, max) {
+  if (item < min) return min;
+  else if (item > max) return max;
+  else return item;
+}
 
+function map(para, orMin, orMax, tarMin, tarMax) {
+  var ratio = (para - orMin) / (orMax - orMin);
+  var tarValue = ratio * (tarMax - tarMin) + tarMin;
+  return tarValue;
+}
+
+function whatever() {
   var path;
-  var paths = [];
-
   paper.setup('myCanvas');
-  var width = document.getElementById('myCanvas').width;
+  var tool = new paper.Tool();
 
-  var tool = new Tool();
+  //making begin and end point
+  var beginP = new paper.Path.Circle(new paper.Point(beginX, beginY), radius);
+  var endP = new paper.Path.Circle(new paper.Point(endX, endY), radius);
+  beginP.strokeColor = 'black';
+  endP.strokeColor = 'black';
 
-  var segment;
+  //making draw area
+  var drawArea = new paper.Path.Rectangle(0, 0, width, height);
+  drawArea.strokeColor = 'black';
+
+  //making interval x path
+  var verticalPaths = [];
+  for (var i = beginX; i <= endX; i += interval) {
+    var verticalPath = new paper.Path.Line(new paper.Point(i, 0), new paper.Point(
+      i, height));
+    //verticalPath.strokeColor = 'black';
+    verticalPaths.push(verticalPath);
+  }
+
+  //making moving ball
+  var boo = new paper.Path.Circle(new paper.Point(width + booPadding, beginY),
+    radius);
+  boo.fillColor = 'blue';
+  console.log(boo.fillColor.hue)
+
+  function getValue() {
+    var value = [];
+    verticalPaths.forEach(function (pa) {
+      var intersections = path.getIntersections(pa);
+      intersections.forEach(function (intersection) {
+        value.push([intersection.point.x, intersection.point.y]);
+      });
+    });
+    return value;
+  }
+
+  var hitSegment;
   var hitOptions = {
     segments: true,
     stroke: true,
@@ -21,35 +74,37 @@ window.onload = function () {
 
   tool.onMouseDown = function (e) {
     if (isDrawingMode) {
-      path = new Path();
-      path.strokeColor = {
-        gradient: {
-          stops: ['blue', 'red', 'yellow']
-        },
-        origin: [0, 0],
-        destination: [width, 0]
-      };
-      path.strokeWidth = 15;
+      if (beginP.bounds.contains(e.point)) {
+        beginP.fillColor = 'blue';
+        path = new paper.Path();
+        path.strokeColor = {
+          gradient: {
+            stops: ['blue', 'green', 'yellow']
+          },
+          origin: [0, 0],
+          destination: [width, 0]
+        };
+        path.strokeWidth = 8;
+        path.add(new paper.Point(beginX, beginY));
+      }
     } else {
-      segment = null;
-      path = null;
-      var hitResult = project.hitTest(e.point, hitOptions);
+      hitSegment = null;
+      var hitResult = paper.project.hitTest(e.point, hitOptions);
       if (!hitResult) {
         return;
       }
       if (e.modifiers.shift) {
         if (hitResult.type === 'segment') {
           hitResult.segment.remove();
-        };
+        }
         return;
       }
       if (hitResult) {
-        path = hitResult.item;
         if (hitResult.type === 'segment') {
-          segment = hitResult.segment;
+          hitSegment = hitResult.segment;
         } else if (hitResult.type === 'stroke') {
           var location = hitResult.location;
-          segment = path.insert(location.index + 1, e.point);
+          hitSegment = path.insert(location.index + 1, e.point);
           path.smooth();
         }
       }
@@ -58,49 +113,53 @@ window.onload = function () {
 
   tool.onMouseDrag = function (e) {
     if (isDrawingMode) {
-      path.add(e.point);
-    } else if (segment) {
-      //console.log(segment.point.x+ " with" + e.delta.x)
-      segment.point.x += e.delta.x;
-      segment.point.y += e.delta.y;
+      if (drawArea.bounds.contains(e.point)) {
+        path.segments.forEach(function (s, index) {
+          if (s.point.x >= e.point.x) {
+            path.removeSegment(index);
+          }
+        });
+        path.add(e.point);
+        if (endP.bounds.contains(e.point)) {
+          endP.fillColor = "yellow";
+        }
+      }
+    } else if (hitSegment) {
+      var index = hitSegment.index;
+      hitSegment.point.x += e.delta.x;
+      hitSegment.point.x = constrain(hitSegment.point.x, path.segments[
+          index - 1].point.x + 0.0000001, path.segments[index + 1].point.x -
+        0.0000001);
+      hitSegment.point.y += e.delta.y;
       path.smooth();
-    } else if (path) {
-      console.log(path)
-      path.position += e.delta;
     }
   };
 
   tool.onMouseUp = function (e) {
     if (isDrawingMode) {
+      if (!endP.bounds.contains(e.point)) {
+        path.add(new paper.Point(endX, endY));
+      }
+      endP.fillColor = 'yellow';
       path.smooth();
       path.simplify();
-      paths.push(path);
+      path.firstSegment.point.x = beginX;
+      path.firstSegment.point.y = beginY;
+      path.lastSegment.point.x = endX;
+      path.lastSegment.point.y = endY;
+      isDrawingDone = true;
     }
   };
 
   tool.onMouseMove = function (e) {
-    if (isDrawingMode) {
-
-    } else {
-      project.activeLayer.selected = false;
+    if (!isDrawingMode) {
+      paper.project.activeLayer.selected = false;
       if (e.item) {
         e.item.selected = true;
       }
     }
   };
 
-  //gatting value
-  function getValue(pathP) {
-    for (var i = 0; i < pathP.curves.length; i++) {
-      var curve = pathP.curves[i];
-      var interval = 5;
-      //console.log(curve.length)
-      for (var j = 1; j <= curve.length; j += interval) {
-        var curvePosition = curve.getLocationAt(j / curve.length);
-        //console.log(curvePosition.point.x, curvePosition.point.y)
-      }
-    }
-  }
   //TODO:
   //0.add begin and end point
 
@@ -112,7 +171,17 @@ window.onload = function () {
 
   //4.generative design? Or just a ball?
 
-  tool.onKeyDown = function (e) {
-    getValue(path)
-  }
+  paper.view.onFrame = function (e) {
+    if (isDrawingDone) {
+      var value = getValue();
+      boo.position.y = value[time][1];
+      boo.fillColor.hue = map(boo.position.y, beginY, endY, 240, 60);
+      time++;
+      if (time >= value.length) time = 0;
+    }
+  };
+}
+
+window.onload = function () {
+  whatever();
 };
